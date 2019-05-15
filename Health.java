185bafddb4606a3a7a073963a4abbcd50dfaa32a
jwong-17 <49002933+jwong-17@users.noreply.github.com>
2019-05-15 14:20:05 -0400

    Create Health.java

---
+++ Health.java
@@ -0,0 +1,18 @@
+
+public class Health {
+
+	private int health;
+	
+	public Health() {
+		health = 0;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setHealth(int h) {
+		health = h;
+	}
+	
+}
